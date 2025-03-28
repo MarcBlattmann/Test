6824139c908365d4853e9da9779cd9716c72ec02
Marc <marc.blattmann08@gmail.com>
2025-03-28 16:56:34 +0100

    Changed to fit content and changed Header

--- src/components/ClubHeader/component.jsx
+++ src/components/ClubHeader/component.jsx
@@ -2,9 +2,9 @@ import "./component.css";
 
 export default function ClubHeader() {
     return (
-      <div className="club-header">
+      <header className="club-header">
         <h1>RedBull Rampage</h1>
         <h3>Herzlich willkommen in unserem Team!</h3>
-      </div>
+      </header>
     );
   }
